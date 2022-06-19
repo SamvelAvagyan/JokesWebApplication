70f2acd8691343cc7c7d89df4a27d50b232cde30
avagyansamvel <avagyansamvel07@gmail.com>
2022-06-19 11:43:53 +0400

    Remove Joke Answer from Index view and add authorization for creating a joke

--- JokesWebApplication/Controllers/JokesController.cs
+++ JokesWebApplication/Controllers/JokesController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using JokesWebApplication.Data;
 using JokesWebApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace JokesWebApplication.Controllers
 {
@@ -54,6 +52,7 @@ namespace JokesWebApplication.Controllers
         }
 
         // GET: Jokes/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
