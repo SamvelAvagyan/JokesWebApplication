1fd7b1cc043651ec0c411bbdcfc24b4a39c2fe51
avagyansamvel <avagyansamvel07@gmail.com>
2022-06-19 11:22:13 +0400

    Add Some Links to navbar and action ShowSearchForm in JokesController

--- JokesWebApplication/Data/Migrations/20220619071126_InitialCreate.Designer.cs
+++ JokesWebApplication/Data/Migrations/20220619071126_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using JokesWebApplication.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JokesWebApplication.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220619071126_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
